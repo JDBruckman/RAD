064dcffe6afa2973594a2bf2eaff9fa674d41f8a
Jonathan Bruckman <96599491+Makomanix@users.noreply.github.com>
2026-01-11 18:15:36 -0500

    added mutations and queries

--- src/components/layout/current-user.tsx
+++ src/components/layout/current-user.tsx
@@ -1,6 +1,7 @@
 import { Popover, Button } from "antd"
 import CustomAvatar from "../custom-avatar"
 import { useGetIdentity } from "@refinedev/core"
+
 import type { User } from '@/graphql/schema.types'
 
 const CurrentUser = () => {
