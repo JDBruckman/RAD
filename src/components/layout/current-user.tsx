56aa653f11c7b10050fd133661ff173e3f5dfc0a
Jonathan Bruckman <96599491+Makomanix@users.noreply.github.com>
2026-01-11 17:54:47 -0500

    created routes/layout/outlet/CurrentUser

---
+++ src/components/layout/current-user.tsx
@@ -0,0 +1,19 @@
+import { Popover, Button } from "antd"
+import CustomAvatar from "../custom-avatar"
+import { useGetIdentity } from "@refinedev/core"
+import type { User } from '@/graphql/schema.types'
+
+const CurrentUser = () => {
+  const { data: user } = useGetIdentity<User>()
+  return (
+    <Popover 
+      placement="bottomRight"
+      trigger="click"
+      styles={{ root: { zIndex: 999}, body: { padding: 0 }}}
+    >
+      <CustomAvatar />
+    </Popover>
+  )
+}
+
+export default CurrentUser
